786d3c5fe8c39ba8eee02f1a56bfc5ba34f53e31
Ana Paula Batiz <ap.batiz@gmail.com>
2023-04-26 23:34:49 -0600

    Se agregan 5 figuras geométricas más y se crea una interfaz para imprimir nombre, área y perímetro de todas las figuras

---
+++ FigurasGeometricas.java
@@ -0,0 +1,9 @@
+
+public interface FigurasGeometricas {
+	
+	// Reglas
+	public String getName();
+	public double calcularArea();
+	public double calcularPerimetro();
+
+} // interface FigurasGeometricas
